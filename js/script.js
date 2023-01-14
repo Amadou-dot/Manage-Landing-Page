7c394d1d85ede2e646d8174ea0d3d6714fadfa9f
Amadou-dot <amadou.sseck@gmail.com>
2023-01-14 01:31:37 -0700

    Small fixes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-const menu_btn = document.getElementById("menu-btn");
-const close_btn = document.getElementById("close-btn");
-const btns = [menu_btn, close_btn];
+const btns = [
+  document.getElementById("menu-btn"),
+  document.getElementById("close-btn"),
+];
 const mobile_nav_el = document.querySelector(".nav__mobile");
-
-// menu_btn.addEventListener("click", function () {
-//   mobile_nav_el.classList.toggle("open");
-// });
 
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
